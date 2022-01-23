d21c02fce52009537eb0a5750a362384a54bfa91
Elencrissy <cristina.furgala@gmail.com>
2022-01-23 20:32:43 +0100

    Set up

---
+++ src/pages/homepage/index.jsx
@@ -0,0 +1,15 @@
+// import styled from "styled-components";
+//
+// const HomePageWrapper = styled.div`
+//   width: 100%;
+// `
+//
+// const HomePage = () => {
+//     return(
+//         <HomePageWrapper>
+//
+//         </HomePageWrapper>
+//     )
+// }
+//
+// export default HomePage
